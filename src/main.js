7dea9f1c1c38b0136ea05cf62588d6e5631f8723
Valmisson Grizorte <missongrizorte@gmail.com>
2019-11-18 23:04:08 -0300

    installed eslint

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ protocol.registerSchemesAsPrivileged([
   }
 ])
 
-function reloadOnChange(win) {
+function reloadOnChange (win) {
   if (!isDevelopment) return { close: () => {} }
 
   const watcher = require('chokidar').watch(path.join(__dirname, '../public/**'), { ignoreInitial: true })
@@ -65,7 +65,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow )
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
