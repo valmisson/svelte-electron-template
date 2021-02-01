d734f7c068552bc6ea207f37e20f0a3722209195
Valmisson Grizorte <missongrizorte@gmail.com>
2021-02-01 16:46:29 -0300

    updating rollup-plugin-svelte compiler options

--- rollup.config.js
+++ rollup.config.js
@@ -22,16 +22,12 @@ export default {
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production
+      },
 
-      emitCss: true,
-
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
-      css: css => {
-        css.write('public/assets/bundle.css')
-      }
+      emitCss: true
     }),
 
     postcss({
